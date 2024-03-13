4eb69b4f5ffabb2c73ea79a0120557ce2ba03270
Maia <matteusmaia889@gmail.com>
2024-03-13 17:26:36 -0300

    utilizando a variável  do style-system

--- App.tsx
+++ App.tsx
@@ -1,16 +1,17 @@
 import { View } from "react-native";
 import { StatusBar } from "react-native";
 import { NativeBaseProvider } from "native-base";
+import { Loading } from "./src/Components/Loading";
+import { THEME } from "./theme/theme";
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
-import { Loading } from "./src/Components/Loading";
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
         translucent
         barStyle={"light-content"}
