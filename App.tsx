7b4dfdac0db48fd3af9cbed4fc8f375ea43a334b
Maia <matteusmaia889@gmail.com>
2024-03-13 16:41:35 -0300

    Adicionando o NativaBaseProvider

--- App.tsx
+++ App.tsx
@@ -1,27 +1,22 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
 import { StatusBar } from "react-native";
+import { NativeBaseProvider } from "native-base";
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
+import { Loading } from "./src/Components/Loading";
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
-    <View
-      style={{
-        alignItems: "center",
-        flex: 1,
-        justifyContent: "center",
-        backgroundColor: "#202024",
-      }}
-    >
-      {fontsLoaded ? <Text>Open world!</Text> : <View />}
+    <NativeBaseProvider>
       <StatusBar
         translucent
         barStyle={"light-content"}
         backgroundColor="transparent"
       />
-    </View>
+      {!fontsLoaded ? <View /> : <Loading />}
+    </NativeBaseProvider>
   );
 }
