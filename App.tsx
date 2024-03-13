5139042e36c9d86ae7689653d9cd413c9e25e290
Maia <matteusmaia889@gmail.com>
2024-03-13 16:13:40 -0300

    Adicionando e estilizando a StatusBar

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import { StatusBar } from "react-native";
 import {
   useFonts,
   Roboto_400Regular,
@@ -6,5 +7,21 @@ import {
 } from "@expo-google-fonts/roboto";
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
-  return <View>{fontsLoaded ? <Text>Open world!</Text> : <View />}</View>;
+  return (
+    <View
+      style={{
+        alignItems: "center",
+        flex: 1,
+        justifyContent: "center",
+        backgroundColor: "#202024",
+      }}
+    >
+      {fontsLoaded ? <Text>Open world!</Text> : <View />}
+      <StatusBar
+        translucent
+        barStyle={"light-content"}
+        backgroundColor="transparent"
+      />
+    </View>
+  );
 }
