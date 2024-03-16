dd40211ad028f73575f594367be05572a216f820
Maia <matteusmaia889@gmail.com>
2024-03-16 00:55:52 -0300

    background do home

--- src/Screens/SignIn.tsx
+++ src/Screens/SignIn.tsx
@@ -1,5 +1,15 @@
 import { VStack, Image } from "native-base";
 import BackgorundImg from "../assets/background.png";
 export function SignIn() {
-  return <VStack flex={1}></VStack>;
+  return (
+    <VStack flex={1} bg="gray.700">
+      <Image
+        source={BackgorundImg}
+        defaultSource={BackgorundImg}
+        alt="Pessoas treinando"
+        resizeMode="contain"
+        position="absolute"
+      />
+    </VStack>
+  );
 }
