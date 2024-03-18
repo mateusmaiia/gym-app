91e1e3c379a50c69b48ef965c271e8f34e60ee36
Maia <matteusmaia889@gmail.com>
2024-03-18 16:25:00 -0300

    Adicionando ScrollView e propriedades a mais para responsividade.

--- src/Screens/SignIn.tsx
+++ src/Screens/SignIn.tsx
@@ -1,4 +1,4 @@
-import { VStack, Image, Text, Center, Heading } from "native-base";
+import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
 import BackgorundImg from "../assets/background.png";
 import LogoSvg from "../assets/logo.svg";
 import { Input } from "../Components/Input";
@@ -6,43 +6,53 @@ import { Button } from "../Components/Button";
 
 export function SignIn() {
   return (
-    <VStack flex={1} bg="gray.700" px={10}>
-      {/* Background */}
-      <Image
-        source={BackgorundImg}
-        defaultSource={BackgorundImg}
-        alt="Pessoas treinando"
-        resizeMode="contain"
-        position="absolute"
-      />
+    // FlexGrow: 1 pra por padrão ocupar todo o tamanho da tela,     junto também com o pb={16} da VStack, para dar mais tamanho a tela.
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <VStack flex={1} bg="gray.700" px={10} pb={16}>
+        {/* Background */}
+        <Image
+          source={BackgorundImg}
+          defaultSource={BackgorundImg}
+          alt="Pessoas treinando"
+          resizeMode="contain"
+          position="absolute"
+        />
 
-      <Center my={24}>
-        <LogoSvg />
-        <Text color="gray.100" fontSize="sm">
-          Treine sua mente e o seu corpo
-        </Text>
-      </Center>
+        <Center my={24}>
+          <LogoSvg />
+          <Text color="gray.100" fontSize="sm">
+            Treine sua mente e o seu corpo
+          </Text>
+        </Center>
 
-      <Center>
-        <Heading
-          color={"gray.100"}
-          fontFamily={"heading"}
-          mb={6}
-          fontSize={"xl"}
-        >
-          Acesse sua conta
-        </Heading>
-      </Center>
-      <Input
-        placeholder="E-mail"
-        keyboardType="email-address"
-        autoCapitalize="none"
-      />
-      <Input placeholder="Senha" secureTextEntry />
+        <Center>
+          <Heading
+            color={"gray.100"}
+            fontFamily={"heading"}
+            mb={6}
+            fontSize={"xl"}
+          >
+            Acesse sua conta
+          </Heading>
+        </Center>
+        <Input
+          placeholder="E-mail"
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+        <Input placeholder="Senha" secureTextEntry />
 
-      <Button title="Acessar" />
-
-      <Button title="Criar conta" variant="outline" />
-    </VStack>
+        <Button title="Acessar" />
+        <Center mt={24}>
+          <Text color="gray.100" fontSize="sm" mb={3} fontFamily="body">
+            Ainda não tem acesso?
+          </Text>
+        </Center>
+        <Button title="Criar conta" variant="outline" />
+      </VStack>
+    </ScrollView>
   );
 }
