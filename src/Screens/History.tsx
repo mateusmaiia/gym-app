8e9fff51d0fa551eaf02b965ad766425fd53d40d
Maia <matteusmaia889@gmail.com>
2024-03-22 22:44:52 -0300

    feat: History

--- src/Screens/History.tsx
+++ src/Screens/History.tsx
@@ -1,9 +1,33 @@
-import { Center, Text } from "native-base";
+import { Center, Heading, VStack, SectionList } from "native-base";
+import { ScreenHeader } from "../Components/ScreenHeader";
+import { HistoryCard } from "../Components/HistoryCard";
+import { useState } from "react";
 
 export function History() {
+  const [exercises, setExercises] = useState([
+    {
+      title: "26.08.22",
+      data: ["Contas", "Puxada frontall"],
+    },
+    {
+      title: "28.08.22",
+      data: ["Puxada frontal", "Remada unilateral"],
+    },
+  ]);
   return (
-    <Center flex={1}>
-      <Text>Home</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Histórico de exercícios" />
+      <SectionList
+        sections={exercises}
+        keyExtractor={(item) => item}
+        renderItem={() => <HistoryCard />}
+        renderSectionHeader={({ section: { title } }) => (
+          <Heading fontSize="sm" mt="8" pb="4" color="gray.100">
+            {title}
+          </Heading>
+        )}
+        px={8}
+      />
+    </VStack>
   );
 }
