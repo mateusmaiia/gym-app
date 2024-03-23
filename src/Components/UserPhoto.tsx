a810a3ddf530eebfb432deb20573811e4325c0f2
Maia <matteusmaia889@gmail.com>
2024-03-22 21:15:18 -0300

    fix: tipo da cor

--- src/Components/UserPhoto.tsx
+++ src/Components/UserPhoto.tsx
@@ -1,16 +1,16 @@
 import { Image, IImageProps } from "native-base";
 
-type UserPhoto = IImageProps & {
+type UserPhotoProps = IImageProps & {
   size: number;
 };
-export function UserPhoto({ size, ...rest }: UserPhoto) {
+export function UserPhoto({ size, ...rest }: UserPhotoProps) {
   return (
     <Image
       w={size}
       h={size}
       rounded="full"
       borderWidth={2}
-      borderColor="gray.400"
+      borderColor="gray.700"
       {...rest}
     />
   );
