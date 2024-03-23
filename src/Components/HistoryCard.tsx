f43be5b73c96c216df48594b20136e7ba7ddd2ce
Maia <matteusmaia889@gmail.com>
2024-03-22 22:45:18 -0300

    feat: create component HistoryCard

---
+++ src/Components/HistoryCard.tsx
@@ -0,0 +1,19 @@
+import { HStack, Heading, Text, VStack } from "native-base";
+
+export function HistoryCard() {
+  return (
+    <HStack px={5} py={4} mb={3} bg="gray.600" alignItems="center" rounded="md">
+      <VStack flex={1}>
+        <Heading color="#fff" fontSize="md" textTransform="capitalize">
+          Costas
+        </Heading>
+        <Text color="gray.100" fontSize="lg" numberOfLines={1}>
+          Puxada frontal
+        </Text>
+      </VStack>
+      <Text color="gray.300" fontSize="md">
+        08:54
+      </Text>
+    </HStack>
+  );
+}
