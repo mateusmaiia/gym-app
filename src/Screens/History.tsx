99b6003cd4deae5d8b2af8ed4f311c558315ad7d
Maia <matteusmaia889@gmail.com>
2024-03-23 15:01:08 -0300

    feat: para caso a sectionList esteja vazia renderizar um frase.

--- src/Screens/History.tsx
+++ src/Screens/History.tsx
@@ -1,4 +1,4 @@
-import { Center, Heading, VStack, SectionList } from "native-base";
+import { Center, Heading, VStack, SectionList, Text } from "native-base";
 import { ScreenHeader } from "../Components/ScreenHeader";
 import { HistoryCard } from "../Components/HistoryCard";
 import { useState } from "react";
@@ -22,9 +22,18 @@ export function History() {
         keyExtractor={(item) => item}
         renderItem={() => <HistoryCard />}
         renderSectionHeader={({ section: { title } }) => (
-          <Heading fontSize="sm" mt="8" pb="4" color="gray.100">
+          <Heading fontSize="sm" mt="10" mb={3} color="gray.100">
             {title}
           </Heading>
+        )}
+        contentContainerStyle={
+          exercises.length === 0 && { justifyContent: "center", flex: 1 }
+        }
+        ListEmptyComponent={() => (
+          <Text color={"gray.100"} textAlign={"center"}>
+            Não há exercícios registrados ainda. {"\n"}
+            Vamos fazer exercícios hoje?
+          </Text>
         )}
         px={8}
       />
