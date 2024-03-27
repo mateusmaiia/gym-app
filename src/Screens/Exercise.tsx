ec256d6de8d7934b04c4d32aa4e051be6b54eba2
Maia <matteusmaia889@gmail.com>
2024-03-27 00:59:27 -0300

    feat/style: icon to goBack

--- src/Screens/Exercise.tsx
+++ src/Screens/Exercise.tsx
@@ -1,9 +1,14 @@
-import { Center, Text } from "native-base";
-
+import { Icon, Text, VStack } from "native-base";
+import { Feather } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native";
 export function Exercise() {
   return (
-    <Center flex={1}>
-      <Text>Home</Text>
-    </Center>
+    <VStack flex={1}>
+      <VStack pt={12} bg="gray.600" pb={8}>
+        <TouchableOpacity>
+          <Icon as={Feather} name="arrow-left" color="green.400" size={6} />
+        </TouchableOpacity>
+      </VStack>
+    </VStack>
   );
 }
