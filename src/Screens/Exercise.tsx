ac1f652100665214bca6301b1a4052ec581f34ef
Maia <matteusmaia889@gmail.com>
2024-03-27 01:01:03 -0300

    feat: function to goBack home

--- src/Screens/Exercise.tsx
+++ src/Screens/Exercise.tsx
@@ -1,11 +1,18 @@
 import { Icon, Text, VStack } from "native-base";
 import { Feather } from "@expo/vector-icons";
 import { TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 export function Exercise() {
+  const navigation = useNavigation();
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
   return (
     <VStack flex={1}>
-      <VStack pt={12} bg="gray.600" pb={8}>
-        <TouchableOpacity>
+      <VStack pt={12} bg="gray.600" px={8}>
+        <TouchableOpacity onPress={handleGoBack}>
           <Icon as={Feather} name="arrow-left" color="green.400" size={6} />
         </TouchableOpacity>
       </VStack>
